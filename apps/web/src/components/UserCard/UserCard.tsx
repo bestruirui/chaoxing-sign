bb02b99b372e49894688337f70494ccc38491351
Lu Zirui <1004279909@qq.com>
2023-04-03 11:48:29 +0000

    文字

--- apps/web/src/components/UserCard/UserCard.tsx
+++ apps/web/src/components/UserCard/UserCard.tsx
@@ -203,7 +203,7 @@ function UserCard(props: UserCardProps) {
           <span className={styles.monitorBtn + ' ' + (props.user.monitor === true ? styles.active : styles.inactive)}
             onClick={handleMonitorChange}
           >
-            {loading ? '加载中' : props.user.monitor === true ? '自动签到' : '自动签到'}
+            {loading ? '加载中...' : props.user.monitor === true ? '自动签到' : '自动签到'}
           </span>
         </CardContent>
       </CardActionArea>
